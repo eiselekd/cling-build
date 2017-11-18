d13aee9f026c071148e2fa8772b6e828a62cd211
Konrad Eisele <Konrad Eisele>
2017-11-18 23:10:07 +0000

    all

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-make all
+export RELEASE_TARBALL=$(ls ${HOME}/ci/build/cling*.tar.bz2)
 
+b=`basename ${RELEASE_TARBALL}`
+d=`expr match $b '\(.*\).tar.bz2`
+
+cp ${RELEASE_TARBALL} archive/
+chmod -R a+rwx archive/
